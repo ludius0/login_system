bf0f369a3d83a3093dd34feb9b8f614ada452d7b
ludius <57571014+ludius0@users.noreply.github.com>
2020-02-10 18:47:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,92 +3,112 @@ from tkinter import messagebox
 #from cryptography.fernet import Fernet
 from getpass import getpass
 import sqlite3
-import os
 
 # Set up main screen
 root = Tk()
 root.title("Login")
-root.geometry("500x500")
-
-# Set up cryptography
-#f = Fernet(key)
+root.geometry("250x200")
 
 # Connect database
-conn = sqlite3.connect('login_database.db')
+conn = sqlite3.connect("login_database.db")
 c = conn.cursor()
 
+# Create new table if there isn't any
+try:
+    c.execute("""CREATE TABLE data (username text, password text, first_name text, last_name text)""")
+except:
+    pass
 
-#Set up non existing database
-if not os.walk("login_database.db"):
-    c.execute('''CREATE TABLE user_data (
-                    username text,
-                    password text,
-                    first_name text,
-                    last_name text
-                    )''')
-
+    
 ### Functions ###
 #Generate new key and database
 def generate_key_and_db():
     pass
 
-# Pop up window with login inputs
-def login():
+def succesful_login(first_name, last_name):
     # Set up sign in screen
     root1 = Tk()
     root1.title("Sign in")
-    root1.geometry("300x400")
+    root1.geometry("150x150")
 
-    # Connect database
-    conn = sqlite3.connect('login_database.db')
-    c = conn.cursor()   
-
-
-    try:
-        pass
-    except Exception as E:
-        pass
-
-    # Close connection to database
-    conn.commit()
-    conn.close()
-
+    # Display first name and last name of username
+    show_label = Label(root1, text=f"Welcome in, {first_name} {last_name}.").pack()
 
     root1.mainloop()
 
+# Pop up window with login inputs
+def login(username_entry, password_entry):
+    statement = False
+
+    #Save username and password from Entry (for better readability)
+    username = username_entry.get()
+    password = password_entry.get()
+    
+    conn = sqlite3.connect('login_database.db')
+    c = conn.cursor()   
+
+    # Loop through database, compare username and password from Entry and get access to login-in window (succesful_login())
+    try:
+        dataCopy = c.execute("SELECT * FROM data")
+        compare = dataCopy.fetchall()
+        for i in compare:
+           if username == i[0] and password == i[1]:
+                statement = True
+                first_name = i[2]
+                last_name = i[3]
+    except:
+        messagebox.showerror("Error", "Something went wrong...")
+
+    succesful_login(first_name, last_name) if statement else messagebox.showerror("Error", "Wrong username or password.")
+
+    conn.commit()
+    conn.close()
+
 # Register to database
-def sign_up_to_db():
-    # Connect database
+def sign_up_to_db(r_username_entry, r_password_entry, r_first_name_entry, r_last_name_entry):
+    statement = True
+    
     conn = sqlite3.connect("login_database.db")
     c = conn.cursor()
 
+    # Convey data (for better readability)
     username = r_username_entry.get()
     password = r_password_entry.get()
     first_name = r_first_name_entry.get()
     last_name = r_last_name_entry.get()
-    
+
+    # Loop through database, compare username and password from Entry and save it to database
+    try:
+        dataCopy = c.execute("SELECT * FROM data")
+        compare = dataCopy.fetchall()
+        for i in compare:
+           if username == i[0] and password == i[1]:
+                statement = False
+    except Exception as e:
+        messagebox.showerror("Error", "Something went wrong...")
 
     # Execute in database
-    c.execute("INSERT INTO user_data VALUES (:username, :password, :first_name, :last_name)",
-                        {
-                            "username": username,
-                            "password": password,
-                            "first_name": first_name,
-                            "last_name": last_name
-                        })
+    if statement:
+        c.execute("INSERT INTO data VALUES (?, ?, ?, ?)", (username, password, first_name, last_name))
+        messagebox.showinfo("Info", "You have been succesfully register.")
+    else:
+        messagebox.showerror("Error", "Somebody has alredy this username/password.")    
 
-    # Close connection to database
+
     conn.commit()
     conn.close()
+
+    # Close window if user get registered
+    root2.destroy() if statement else None
     
 
 # Pop up window with register inputs
 def register():
-    global r_username_entry, r_password_entry, r_first_name_entry, r_last_name_entry
+    global root2
     #Set up register screen
     root2 = Tk()
     root2.title("Sign in")
-    root2.geometry("300x400")
+    root2.geometry("250x250")
 
 
     ### Labels, Entry and Button
@@ -99,25 +119,20 @@ def register():
     
     r_username_entry = Entry(root2)
     r_username_entry.grid(row=0, column=1)
-    r_password_entry = Entry(root2)
+    r_password_entry = Entry(root2, show="*")
     r_password_entry.grid(row=1, column=1)
     r_first_name_entry = Entry(root2)
     r_first_name_entry.grid(row=2, column=1)
     r_last_name_entry = Entry(root2)
     r_last_name_entry.grid(row=3, column=1)
 
-    sign_up = Button(root2, text="Register to database", command=sign_up_to_db).grid(row=4, column=1)
+    sign_up = Button(root2, text="Register to database", command=lambda:sign_up_to_db(r_username_entry, r_password_entry, r_first_name_entry, r_last_name_entry)).grid(row=4, column=1)
     ###
-
-    # Loop
+    
     root2.mainloop()
 
-### Functions ###
 
-
-
-
-
+### MAIN LABELS, ENTRY AND BUTTONS
 # Labels
 username_label = Label(root, text="Username:").grid(row=0, column=0)
 password_label = Label(root, text="Password:").grid(row=1, column=0)
@@ -125,14 +140,16 @@ password_label = Label(root, text="Password:").grid(row=1, column=0)
 # Entry
 username_entry = Entry(root)
 username_entry.grid(row=0, column=1)
-password_entry = Entry(root)
+password_entry = Entry(root, show="*")
 password_entry.grid(row=1, column=1)
 
 # Buttons
-sign_in_b = Button(root, text="Sign In", command=login).grid(row=2, column=1)
+sign_in_b = Button(root, text="Sign In", command=lambda:login(username_entry, password_entry)).grid(row=2, column=1)
 register_in = Button(root, text="Register", command=register).grid(row=3, column=1)
 restart = Button(root, text="Generate new\n database and key", command=generate_key_and_db).grid(row=4, column=1)
 
+
+### CLOSE ALL
 # Close connection to database
 conn.commit()
 conn.close()
